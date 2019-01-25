2a558aeb239faeacf413d223dbadc6f64ba949b9
Su Su Aung <susuaung@Sus-MacBook-Pro.local>
2019-01-26 00:29:24 +0800

    commit vehicle css

--- src/app/components/vehicle-detail.component.ts
+++ src/app/components/vehicle-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StarWarsSvc } from '../starwars.service';
 import { ActivatedRoute } from '@angular/router';
-import {Vehicles} from '../interface';
+import { Vehicles } from '../interface';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -11,28 +11,27 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./vehicle-detail.component.css']
 })
 export class VehicleDetailComponent implements OnInit {
- 
+
   constructor(private starWarSvc: StarWarsSvc, private http: HttpClient,
-    private activatedRoute: ActivatedRoute){}
+    private activatedRoute: ActivatedRoute) { }
 
-    vehicle: Vehicles = null;
-    id: number;
-
+  vehicle: Vehicles = null;
+  id: number;
   ngOnInit() {
-this.id = this.activatedRoute.snapshot.params.id;
-const item = "Vehicles";
-this.starWarSvc.getDetails(item,this.id)
-    .then(result=> {
+    this.id = this.activatedRoute.snapshot.params.id;
+    const item = "Vehicles";
+    this.starWarSvc.getDetails(item, this.id)
+      .then(result => {
         this.vehicle = result;
-        if(result.pilots != null || result.pilots !=[]){
-          this.vehicle.pilots=this.starWarSvc.getPeopleNames(result.pilots);
+        if (result.pilots != null || result.pilots != []) {
+          this.vehicle.pilots = this.starWarSvc.getPeopleNames(result.pilots);
         }
-        if(result.films != null || result.films != []){
+        if (result.films != null || result.films != []) {
           this.vehicle.fimls = this.starWarSvc.getFilmNames(result.films);
         }
-    })
-    .catch(err=> {
-      console.error("Vehicles Error: ", err);
-    })
+      })
+      .catch(err => {
+        console.error("Vehicles Error: ", err);
+      })
   }
 }
